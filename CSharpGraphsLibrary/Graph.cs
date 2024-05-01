ef2cfd40242a370fda7a1bcfa722bbc56f249720
Andrey Dorofeev <aria17.mail@gmail.com>
2024-05-01 19:19:26 +0300

    Corrected Graph class and tested it.

--- CSharpGraphsLibrary/Graph.cs
+++ CSharpGraphsLibrary/Graph.cs
@@ -18,61 +18,58 @@
         public static Graph<T> Create(IEnumerable<T> vertices)
         {
             if (vertices is null) throw new ArgumentNullException(
-                "Create() failed. Specified enumerable collection of vertices was null.");
+                "Specified enumerable collection of vertices was null.");
             return new Graph<T>(vertices);
         }
         public static Graph<T> Create(params T[] vertices) => new(vertices);
         public void AddVertex(T v)
         {
             if (mapping.ContainsKey(v)) throw new InvalidOperationException(
-                "AddVertex() failed. Specified vertex was already in the graph.");
+                "Specified vertex was already in the graph.");
             mapping.Add(v, new());
         }
-        public bool TryAddVertex(T v) => mapping.TryAdd(v, new());
         public void RemoveVertex(T v)
         {
-            if (mapping.Remove(v))
-            {
-                foreach (T other in mapping.Keys) mapping[other].Remove(v);
-                return;
-            }
-            throw new InvalidOperationException(
-                "RemoveVertex() failed. Specified vertex was not in the graph.");
-        }
-        public bool TryRemoveVertex(T v)
-        {
-            if (mapping.Remove(v))
-            {
-                foreach (T other in mapping.Keys) mapping[other].Remove(v);
-                return true;
-            }
-            return false;
+            if (!mapping.ContainsKey(v)) throw new InvalidOperationException(
+                "Specified vertex was not in the graph.");
+            foreach (T neighbourForward in mapping[v]) Disconnect(v, neighbourForward, false);
+            foreach (T neighbourBackward in mapping.Keys) Disconnect(neighbourBackward, v, true);
+            mapping.Remove(v);
         }
         public bool HasVertex(T vertex) => mapping.ContainsKey(vertex);
-        public void AddEdge(T v1, T v2, bool orientedEdge)
+        public void Connect(T v1, T v2, bool orientedEdge)
         {
             if (!mapping.ContainsKey(v1)) throw new InvalidOperationException(
-                $"AddEdge() failed. Vertex {v1} was not in the graph.");
+                $"Vertex {v1} was not in the graph.");
             if (!mapping.ContainsKey(v2)) throw new InvalidOperationException(
-                $"AddEdge() failed. Vertex {v2} was not in the graph.");
-            if (orientedEdge) mapping[v1].Add(v2);
+                $"Vertex {v2} was not in the graph.");
+            if (orientedEdge)
+            {
+                if (mapping[v1].Add(v2) && !mapping[v2].Contains(v1)) ++EdgeCount;
+            }
             else
             {
-                mapping[v1].Add(v2);
-                mapping[v2].Add(v1);
+                if (mapping[v1].Add(v2) & mapping[v2].Add(v1)) ++EdgeCount;
             }
         }
-        public void RemoveEdge(T v1, T v2, bool orientedEdge)
+        public void Disconnect(T v1, T v2, bool orientedEdge)
         {
             if (!mapping.ContainsKey(v1)) throw new InvalidOperationException(
-                $"RemoveEdge() failed. Vertex {v1} was not in the graph.");
+                $"Vertex {v1} was not in the graph.");
             if (!mapping.ContainsKey(v2)) throw new InvalidOperationException(
-                $"RemoveEdge() failed. Vertex {v2} was not in the graph.");
-            if (orientedEdge) mapping[v1].Remove(v2);
+                $"Vertex {v2} was not in the graph.");
+            if (orientedEdge)
+            {
+                if (mapping[v1].Remove(v2) && !mapping[v2].Contains(v1)) --EdgeCount;
+            }
             else
             {
-                mapping[v1].Remove(v2);
-                mapping[v2].Remove(v1);
+                bool forward = mapping[v1].Remove(v2), backward = mapping[v2].Remove(v1);
+                if (forward)
+                {
+                    if (backward || !mapping[v2].Contains(v1)) --EdgeCount;
+                }
+                else if (backward) --EdgeCount;
             }
         }
         public bool HasEdge(T v1, T v2, bool orientedEdge)
@@ -80,6 +77,12 @@
             if (!mapping.ContainsKey(v1) || !mapping.ContainsKey(v2)) return false;
             if (orientedEdge) return mapping[v1].Contains(v2);
             return mapping[v1].Contains(v2) & mapping[v2].Contains(v1);
+        }
+        public HashSet<T> NeighboursOf(T v)
+        {
+            if (!mapping.ContainsKey(v)) throw new InvalidOperationException(
+                $"Vertex {v} was not in the graph.");
+            return mapping[v];
         }
         public IEnumerable<T> Vertices()
         {
@@ -92,12 +95,12 @@
             {
                 foreach (T neighbour in mapping[v])
                 {
-                    if (edges.Contains((v, neighbour, false)))
+                    if (edges.Contains((neighbour, v, true)))
                     {
-                        edges.Remove((v, neighbour, false));
-                        edges.Add((neighbour, v, true));
+                        edges.Remove((neighbour, v, true));
+                        edges.Add((neighbour, v, false));
                     }
-                    else edges.Add((v, neighbour, false));
+                    else edges.Add((v, neighbour, true));
                 }
             }
             return edges;
--- CSharpGraphsTests/GraphTests.cs
+++ CSharpGraphsTests/GraphTests.cs
@@ -1,6 +1,182 @@
-﻿namespace CSharpGraphsTests
+﻿using NUnit.Framework;
+using CSharpGraphsLibrary;
+namespace CSharpGraphsTests
 {
+    [TestFixture]
     public class GraphTests
     {
+        [Test]
+        public void Create()
+        {
+            var graph = Graph<int>.Create();
+            Assert.That(graph.EdgeCount == 0 &
+                        graph.VertexCount == 0 &
+                        !graph.Vertices().Any() &
+                        !graph.Edges().Any());
+        }
+        [Test]
+        public void CreateWithEnumerableThrowsOnNull()
+        {
+            IEnumerable<int> enumerable = null!;
+            Assert.Throws<ArgumentNullException>(() => { var graph = Graph<int>.Create(enumerable); });
+        }
+        [Test]
+        public void CreateWithEnumerable()
+        {
+            HashSet<int> vertices = new() { 1, 2, 3, 4, 5 };
+            var graph = Graph<int>.Create(vertices);
+            IEnumerable<int> verts = graph.Vertices();
+            Assert.That(graph.EdgeCount == 0 &
+                        graph.VertexCount == 5 &
+                        verts.Contains(1) &
+                        verts.Contains(2) &
+                        verts.Contains(3) &
+                        verts.Contains(4) &
+                        verts.Contains(5) &
+                        !graph.Edges().Any());
+        }
+        [Test]
+        public void CreateWithParams()
+        {
+            var graph = Graph<int>.Create(1, 2, 3, 4, 5);
+            IEnumerable<int> verts = graph.Vertices();
+            Assert.That(graph.EdgeCount == 0 &
+                        graph.VertexCount == 5 &
+                        verts.Contains(1) &
+                        verts.Contains(2) &
+                        verts.Contains(3) &
+                        verts.Contains(4) &
+                        verts.Contains(5) &
+                        !graph.Edges().Any());
+        }
+        [Test]
+        public void AddVertexThrowsOnSame()
+        {
+            var graph = Graph<int>.Create(1);
+            Assert.Throws<InvalidOperationException>(() => graph.AddVertex(1));
+        }
+        [Test]
+        public void ConnectThrowsOnFirstMissing()
+        {
+            var graph = Graph<int>.Create(1, 2);
+            Assert.Throws<InvalidOperationException>(() => graph.Connect(3, 1, true));
+        }
+        [Test]
+        public void ConnectThrowsOnSecondMissing()
+        {
+            var graph = Graph<int>.Create(1, 2);
+            Assert.Throws<InvalidOperationException>(() => graph.Connect(1, 3, false));
+        }
+        [Test]
+        public void Connect1()
+        {
+            var graph = Graph<int>.Create(1, 2, 3);
+            graph.Connect(1, 2, true);
+            if (graph.EdgeCount != 1) Assert.Fail();
+            graph.Connect(2, 1, true);
+            if (graph.EdgeCount != 1) Assert.Fail();
+            graph.Connect(2, 3, false);
+            if (graph.EdgeCount != 2) Assert.Fail();
+            graph.Connect(2, 3, true);
+            graph.Connect(3, 2, true);
+            if (graph.EdgeCount != 2) Assert.Fail();
+            Assert.Pass();
+        }
+        [Test]
+        public void Connect2()
+        {
+            var graph = Graph<int>.Create(1, 2, 3, 4, 5, 6);
+            graph.Connect(1, 2, true);
+            if (graph.EdgeCount != 1) Assert.Fail();
+            graph.Connect(1, 5, false);
+            if (graph.EdgeCount != 2) Assert.Fail();
+            graph.Connect(2, 6, true);
+            if (graph.EdgeCount != 3) Assert.Fail();
+            graph.Connect(2, 4, false);
+            if (graph.EdgeCount != 4) Assert.Fail();
+            graph.Connect(5, 3, true);
+            if (graph.EdgeCount != 5) Assert.Fail();
+            graph.Connect(4, 3, false);
+            var edges = (HashSet<(int, int, bool)>)graph.Edges();
+            if (graph.EdgeCount != 6 || edges.Count != 6 ||
+                !edges.Contains((1, 2, true)) ||
+                !edges.Contains((1, 5, false)) ||
+                !edges.Contains((2, 6, true)) ||
+                !edges.Contains((2, 4, false)) ||
+                !edges.Contains((5, 3, true)) ||
+                !edges.Contains((3, 4, false))) Assert.Fail();
+            Assert.Pass();
+        }
+        [Test]
+        public void Disconnect()
+        {
+            var graph = Graph<int>.Create(1, 2, 3);
+            graph.Connect(1, 2, false);
+            if (graph.EdgeCount != 1) Assert.Fail();
+            graph.Disconnect(1, 2, true);
+            if (graph.EdgeCount != 1) Assert.Fail();
+            graph.Disconnect(2, 1, true);
+            if (graph.EdgeCount != 0) Assert.Fail();
+            graph.Connect(2, 3, false);
+            if (graph.EdgeCount != 1) Assert.Fail();
+            graph.Disconnect(3, 2, false);
+            if (graph.EdgeCount != 0) Assert.Fail();
+            Assert.Pass();
+        }
+        [Test]
+        public void RemoveVertex()
+        {
+            var graph = Graph<int>.Create(1, 2, 3, 4, 5);
+            graph.Connect(1, 2, true);
+            graph.Connect(3, 1, true);
+            graph.Connect(1, 4, false);
+            graph.Connect(5, 1, false);
+            if (graph.EdgeCount != 4) Assert.Fail();
+            graph.RemoveVertex(1);
+            if (graph.EdgeCount != 0) Assert.Fail();
+            if (graph.Edges().Any()) Assert.Fail();
+            Assert.Pass();
+        }
+        [Test]
+        public void HasVertex()
+        {
+            var graph = Graph<int>.Create(1);
+            Assert.That(graph.HasVertex(1) && !graph.HasVertex(2));
+        }
+        [Test]
+        public void HasEdge()
+        {
+            var graph = Graph<int>.Create(1, 2, 3);
+            graph.Connect(1, 2, true);
+            graph.Connect(1, 3, false);
+            Assert.That(graph.HasEdge(1, 2, true) &&
+                        !graph.HasEdge(1, 2, false) &&
+                        !graph.HasEdge(2, 1, true) &&
+                        graph.HasEdge(1, 3, true) &&
+                        graph.HasEdge(3, 1, true) &&
+                        graph.HasEdge(1, 3, false) &&
+                        graph.HasEdge(3, 1, false));
+        }
+        [Test]
+        public void NeighboursOf()
+        {
+            var graph = Graph<int>.Create(1, 2, 3, 4);
+            graph.Connect(1, 2, true);
+            graph.Connect(1, 3, true);
+            graph.Connect(1, 4, false);
+            graph.Connect(2, 1, true);
+            HashSet<int> neighboursOf1 = graph.NeighboursOf(1);
+            Assert.That(neighboursOf1.Contains(2) &&
+                        neighboursOf1.Contains(3) &&
+                        neighboursOf1.Contains(4));
+        }
+        [Test]
+        public void Vertices()
+        {
+            var graph = Graph<int>.Create(1, 2, 3, 4, 5, 0, -1);
+            var vertices = graph.Vertices();
+            for (int i = -1; i < 6;) if (!vertices.Contains(i++)) Assert.Fail();
+            Assert.Pass();
+        }
     }
 }
